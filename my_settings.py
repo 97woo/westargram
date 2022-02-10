77074cf46a51292c52e0fa37832a0e78bf9757a3
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-02-10 21:43:51 +0900

    first commit

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'practice1',
+        'USER': 'root',
+        'PASSWORD': 'asd3689',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+
+SECRET_KEY = 'django-insecure-r-n2ne&9gr_#)caf)ew9l@j^+e&4))27%wa#xp!t=(773yf7g7'
